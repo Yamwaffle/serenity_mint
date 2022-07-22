df987f8dbd0e20ad278836d3d280c18f66a6a54b
Wenyen333 <73871351+Wenyen333@users.noreply.github.com>
2022-07-22 09:04:59 -0700

    Add files via upload

--- Content/js/serenity_mint.js
+++ Content/js/serenity_mint.js
@@ -14,77 +14,84 @@ $(document).ready(function()
 
     // reads from mint_config.xml and stores data
     //var xmlDoc = $.parseXML("<collections> <collection> <name>mūz alias: the dream girl</name> <cost>0.06</cost>  <total>1000</total>  <layout>right</layout>  <foldername>dream_girl</foldername> <backgrounds>   <background> <filename>background_1.jpg</filename> <starttime>5</starttime>  <endtime>13</endtime>  </background> <background> <filename>background_2.jpg</filename> <starttime>13</starttime> <endtime>21</endtime> </background> <background> <filename>background_3.jpg</filename> <starttime>21</starttime> <endtime>5</endtime> </background> </backgrounds> <characters>   <character> <filename>dream_1.png</filename> </character> <character> <filename>dream_2.png</filename> </character> <character> <filename>dream_3.png</filename> </character> <character> <filename>dream_4.png</filename> </character> </characters> </collection> </collections>");
-    var xmlDoc = $.parseXML("../files/mint_config.xml");
-    var $xml = $(xmlDoc);
+    //var xmlDoc = $.parseXML("../files/mint_config.xml");
+    $.ajax({
+        type: 'GET',
+        url: '../files/mint_config.xml',
+        dataType: 'xml',    
+        success: function(xml) {
+            var $collections = $(xml).find("collections");
+            var currentHour = (new Date()).getHours();
 
-    var $collections = $xml.find("collections");
-    var currentHour = (new Date()).getHours();
-
-    $collections.each(function(){
-        var name = $(this).find('name').text(),
-            cost = $(this).find('cost').text(),
-            total = $(this).find('total').text(),
-            layout = $(this).find('layout').text(),
-            folderName = $(this).find('foldername').text(),
-            $backgrounds = $(this).find("backgrounds"),
-            $characters = $(this).find("characters");
+            $collections.each(function(){
+                var name = $(this).find('name').text(),
+                    cost = $(this).find('cost').text(),
+                    total = $(this).find('total').text(),
+                    layout = $(this).find('layout').text(),
+                    folderName = $(this).find('foldername').text(),
+                    $backgrounds = $(this).find("backgrounds"),
+                    $characters = $(this).find("characters");
 
 
-        // sets the background according to user's system time
-        $backgrounds.each(function(){
+                // sets the background according to user's system time
+                $backgrounds.each(function(){
 
-            var $background = $(this).find('background');
+                    var $background = $(this).find('background');
 
-            $background.each(function(){
-                var startTime = parseInt($(this).find('starttime').text()),
-                endTime = parseInt($(this).find('endtime').text()),
-                fileName = $(this).find('filename').text();
+                    $background.each(function(){
+                        var startTime = parseInt($(this).find('starttime').text()),
+                        endTime = parseInt($(this).find('endtime').text()),
+                        fileName = $(this).find('filename').text();
 
-                // checks if there's no start or end time (which means only 1 background)
-                if(isNaN(startTime)) {
-                    setBackground("url('./Content/images/" + folderName + "/" + fileName + "')");
-                }
+                        // checks if there's no start or end time (which means only 1 background)
+                        if(isNaN(startTime)) {
+                            setBackground("url('./Content/images/" + folderName + "/" + fileName + "')");
+                        }
 
-                if (endTime < startTime) {
-                    endTime = endTime + 24;
-                }
+                        if (endTime < startTime) {
+                            endTime = endTime + 24;
+                        }
 
-                // updates background according to user system time of day
-                if (currentHour >= startTime && currentHour < endTime) {
-                    setBackground("url('./Content/images/" + folderName + "/" + fileName + "')");
-                }
-            });
-        });
-
-        
-        // randomly chooses character to display (if more than one)
-        $characters.each(function(){
-            var numberOfCharacters = $(this).children("character").length;
-
-            // if there are multiple to choose from
-            if (numberOfCharacters > 1) {
-                var randomCharacter = Math.floor(Math.random() * numberOfCharacters) + 1;
-                var currentCount = 1;
-                $characters.find("character").each(function() {
-                    if(currentCount === randomCharacter)
-                    {
-                        // set character
-                        var fileName = $(this).find('filename').text();
-                        setCharacter("./Content/images/" + folderName + "/" + fileName);
-                        return false;
-                    } else {
-                        currentCount++;
-                    }
+                        // updates background according to user system time of day
+                        if (currentHour >= startTime && currentHour < endTime) {
+                            setBackground("url('./Content/images/" + folderName + "/" + fileName + "')");
+                        }
+                    });
                 });
 
-            } else {
-                // just set the one character
-                var fileName = $(this).find('filename').text();
-                setCharacter("./Content/images/" + folderName + "/" + fileName);
-            }
-        });
-    });
+                
+                // randomly chooses character to display (if more than one)
+                $characters.each(function(){
+                    var numberOfCharacters = $(this).children("character").length;
 
+                    // if there are multiple to choose from
+                    if (numberOfCharacters > 1) {
+                        var randomCharacter = Math.floor(Math.random() * numberOfCharacters) + 1;
+                        var currentCount = 1;
+                        $characters.find("character").each(function() {
+                            if(currentCount === randomCharacter)
+                            {
+                                // set character
+                                var fileName = $(this).find('filename').text();
+                                setCharacter("./Content/images/" + folderName + "/" + fileName);
+                                return false;
+                            } else {
+                                currentCount++;
+                            }
+                        });
+
+                    } else {
+                        // just set the one character
+                        var fileName = $(this).find('filename').text();
+                        setCharacter("./Content/images/" + folderName + "/" + fileName);
+                    }
+                });
+            });
+        },
+        error: function(err){
+            alert("error occurred reading xml: " + err);
+        }
+    });
 });
 
 
